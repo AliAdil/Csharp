fd7756c35b97d6bfd8c94c88fcbe9a698e0fd3ae
aliadil <aliadilg@gmail.com>
2018-02-16 01:08:30 +0500

    update

--- Csharp/MainPage.xaml.cs
+++ Csharp/MainPage.xaml.cs
@@ -27,41 +27,26 @@ namespace Csharp
             this.InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void button1_Click(object sender, RoutedEventArgs e)
         {
-            int number = 15;
-            number = number + 10;
-            number = 36 * 15;
-            number = 12 - (42 / 7);
-            number += 10;
-            number *= 3;
-            number = 71 / 3;
-
-            int count = 0;
-            count++;
-            count--;
-
-            string result = "hello";
-            result += " again " + result;
-           // output.Test = result;
-            result = "the value is: " + count;
-            result = "";
-
-            bool yesNo = false;
-            bool anotherBool = true;
-            yesNo = !anotherBool;
-
-            int someValue = 24;
-            string message = "";
-            if (someValue == 24)
-            {
-                message = "The Value is 24";
-            }
-            else
-            {
-                message = "The Value Wasnt 24";
-            }
 
         }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        
     }
 }
