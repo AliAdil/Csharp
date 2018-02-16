3edbaf8ef6efda713b1f1ac9008b2d1a40921bde
aliadil <aliadilg@gmail.com>
2018-02-17 04:40:28 +0500

    update

--- WindowsForm/FlashyThing.Designer.cs
+++ WindowsForm/FlashyThing.Designer.cs
@@ -29,11 +29,13 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.test = new System.Windows.Forms.Label();
+            this.multi = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(81, 74);
+            this.button1.Location = new System.Drawing.Point(87, 162);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(95, 74);
             this.button1.TabIndex = 0;
@@ -41,20 +43,42 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // test
+            // 
+            this.test.Location = new System.Drawing.Point(79, 41);
+            this.test.Name = "test";
+            this.test.Size = new System.Drawing.Size(147, 62);
+            this.test.TabIndex = 1;
+            this.test.Text = "What is my Name";
+            // 
+            // multi
+            // 
+            this.multi.AutoSize = true;
+            this.multi.Location = new System.Drawing.Point(24, 121);
+            this.multi.Name = "multi";
+            this.multi.Size = new System.Drawing.Size(23, 13);
+            this.multi.TabIndex = 2;
+            this.multi.Text = "2*3";
+            // 
             // FlashyThing
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.multi);
+            this.Controls.Add(this.test);
             this.Controls.Add(this.button1);
             this.Name = "FlashyThing";
             this.Text = "FlashyThing";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label test;
+        private System.Windows.Forms.Label multi;
     }
 }
--- WindowsForm/FlashyThing.cs
+++ WindowsForm/FlashyThing.cs
@@ -19,22 +19,30 @@ namespace WindowsForm
 
         private void button1_Click(object sender, EventArgs e)
         {
-            while (true)
+            int a=2 , b= 3;
+            int d = a*b;
+            String y = d.ToString();
+           
+            multi.Text = y;
+            test.Text = "My Name is Tofeeq";
+            while(Visible== true)
             {
                 
             
-            for(int c = 0; c <254 ; c++){
+            for(int c = 0; c <254 && Visible ; c++){
             this.BackColor = Color.FromArgb(c, 255-c, c);
             Application.DoEvents();
             System.Threading.Thread.Sleep(3);
             }
 
-            for (int c = 253; c > 0; c--)
+            for (int c = 253; c > 0 && Visible ; c--)
             {
                 this.BackColor = Color.FromArgb(c, 255 - c, c);
                 Application.DoEvents();
                 System.Threading.Thread.Sleep(3);
             }
+
+           
             }
         }
     }
