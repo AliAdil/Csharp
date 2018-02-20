fe4db6c62126ef83fde758244b7dd43d147e5dc9
aliadil <aliadilg@gmail.com>
2018-02-20 05:52:17 +0500

    Pool Puzzle Solved

---
+++ Talker/PoolPuzzle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Talker
+{
+    public partial class PoolPuzzle : Form
+    {
+        public PoolPuzzle()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string result = "";
+            Echo e1 = new Echo();
+            Echo e2 = e1;
+            
+
+            int x = 0 ;
+            while (x < 4){
+                result = result + e1.Hello()+ "\n";
+
+                if(x>0){
+                    e2.count = e2.count + 1;
+                }
+                if (x>1){
+                    e2.count = e2.count + e1.count;
+                }
+
+                x = x +1;
+
+            }
+            MessageBox.Show(result + "count: " + e2.count);
+        }
+    }
+
+        class Echo {
+
+            public int count  = 0;
+              public string  Hello () {
+                return "helloooo...";
+            }
+        }
+     
+    
+}
