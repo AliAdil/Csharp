2568920089efd4e26c21401b375863d3428c558e
aliadil <aliadilg@gmail.com>
2018-02-21 01:25:42 +0500

    Give Cash

---
+++ WindowsForm/Guy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace WindowsForm
+{
+    class Guy
+    {
+        public string Name;
+        public int cash;
+
+        public int GiveCash(int amount)
+        {
+            if (amount <= cash && amount > 0)
+            {
+                cash -= amount;
+                return amount;
+            }
+            else
+            {
+                MessageBox.Show("I don't have enough cash to give you" + amount , Name + " Says..");
+                return 0;
+            }
+        }
+    }
+}
